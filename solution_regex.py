428b1b1d1c654141b0852f71689eb36c9a400471
Anton <anxton4@gmail.com>
2022-06-10 16:43:14 +0200

    regex update

--- solution_regex.py
+++ solution_regex.py
@@ -2,4 +2,4 @@ import re
 def erase(cc):
     # supprime : espace seul en début de ligne | espace entre deux lettres | espace seul en fin de ligne
     # utilisation de lookbehinds/lookaheads pour éviter les problèmes de chevauchement
-    return re.sub(r'^ (?=[^ ])|(?<=[^ ]) (?=[^ ])|(?<=[^ ]) $', '', cc)
+    return re.sub(r'(?<! ) (?! )', '', cc)
