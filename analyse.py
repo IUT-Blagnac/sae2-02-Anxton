7324c61473ed7b0837878c782b3ed0318cb5cc28
Anton <anxton4@gmail.com>
2022-06-10 16:08:33 +0200

    changement anaylse

--- analyse.py
+++ analyse.py
@@ -8,12 +8,17 @@ from solution_suppr import erase as erase_suppr
 from solution_ajout import erase as erase_ajout
 from solution_recursif import erase as erase_recursif
 
-s = " le. Pre  lude a  ttdu je du    ient "
-nb = 1000000
+
+import string
+import random
+s = ""
+for i in range(100000):
+    s+=random.choice(string.ascii_letters+"     ")
+nb = 100
 print("solution_regex:",timeit.timeit(lambda: erase_regex(s), number=nb))
 print("solution_compteur:",timeit.timeit(lambda: erase_compteur(s), number=nb))
 print("solution_comprehension:",timeit.timeit(lambda: erase_comprehension(s), number=nb))
 print("solution_ajout:",timeit.timeit(lambda: erase_ajout(s), number=nb))
 print("solution_while:",timeit.timeit(lambda: erase_while(s), number=nb))
 print("solution_suppr:",timeit.timeit(lambda: erase_suppr(s), number=nb))
-print("solution_recursif:",timeit.timeit(lambda: erase_recursif(s), number=nb)) 
+# print("solution_recursif:",timeit.timeit(lambda: erase_recursif(s), number=nb)) 
