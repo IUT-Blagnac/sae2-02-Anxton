cc62d898eb8779e9780e28b1175e45713bc54267
Anxton <anxton4@gmail.com>
2022-06-10 18:29:52 +0200

    Resolve phase1 : [Efficacité] [Python]

--- solution_regex.py
+++ solution_regex.py
@@ -1,4 +1,5 @@
 import re
+
 def erase(cc):
     # supprime : espace seul en début de ligne | espace entre deux lettres | espace seul en fin de ligne
     # utilisation de lookbehinds/lookaheads pour éviter les problèmes de chevauchement
