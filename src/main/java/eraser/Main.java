737c2df06dd3c3ab22391cc263600d41b5092433
Anxton <anxton4@gmail.com>
2022-06-10 23:57:26 +0200

    Resolve phase1 : [Efficacité] [Java]

--- src/main/java/eraser/Main.java
+++ src/main/java/eraser/Main.java
@@ -3,6 +3,6 @@ package eraser;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Eraser.erase(args[1]));
+        System.out.println(Eraser.erase("a bc"));
     }
 }
