a97d18c21ce0b4c6bf8ec4a60561ee13017b2b22
Anxton <anxton4@gmail.com>
2022-06-16 23:39:41 +0200

    fichier tests python

--- analyse/verification_python.py
+++ analyse/verification_python.py
@@ -1,13 +1,37 @@
-from algos import efficacite21 as eff21
-from algos import efficacite46 as eff46
-from algos import simplicite86 as simp86
-from algos import simplicite118 as simp118
-from algos import simplicite154 as simp154
-from algos import simplicite160 as simp160
-from algos import simplicite169 as simp169
-from algos import sobriete102 as sob102
+from algos.efficacite21 import erase as eff21
+from algos.efficacite46 import erase2 as eff46
+from algos.simplicite86 import enlever_blanc as simp86
+from algos.simplicite118 import erase as simp118
+from algos.simplicite154 import erase as simp154
+from algos.simplicite160 import bonmot2 as simp160
+from algos.simplicite169 import erase as simp169
+from algos.sobriete102 import erase as sob102
 
+def sample_tests(erase):
+    try:
+        ok = True
+        if erase('') != '': ok = False
+        if erase('   06   07 65 19 70 ') != '   06   07651970': ok = False
+        if erase(' 666, the number of the beast  ') != '666,thenumberofthebeast  ': ok = False
+        if erase('  Cou cou  J M  B') != '  Coucou  JM  B': ok = False
+        if ok: print('✅') # Fonctionne !
+        else: print('❌') # Ne fonctionne pas comme attendu
+    except:
+        print("💀") # Erreur levée
 
-
-
-print("|"+eff21.erase("   a     a a  ") + "|")
+print("efficacite21:")
+sample_tests(eff21)
+print("efficacite46:")
+sample_tests(eff46)
+print("simplicite86:")
+sample_tests(simp86)
+print("simplicite118:")
+sample_tests(simp118)
+print("simplicite154:")
+sample_tests(simp154)
+print("simplicite160:")
+sample_tests(simp160)
+print("simplicite169:")
+sample_tests(simp169)
+print("sobriete102:")
+sample_tests(sob102)
