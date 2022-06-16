f76ec76337e7f845d6fc488ca8b6714c6f7d6394
Anxton <anxton4@gmail.com>
2022-06-17 01:30:42 +0200

    verification C

---
+++ analyse/verification.c
@@ -0,0 +1,9 @@
+#include "algos/sobriete76.h"
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("sobriete76:\n");
+    printf("|%s|", erase("  bo  n jour  "));
+    return 0;
+}
