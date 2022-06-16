b38b37a730fad1c96e002b373ec3e9747edf4f72
Anxton <anxton4@gmail.com>
2022-06-16 23:19:35 +0200

    rename + move + debut verification

---
+++ analyse/verification_python.py
@@ -0,0 +1,13 @@
+from algos import efficacite21 as eff21
+from algos import efficacite46 as eff46
+from algos import simplicite86 as simp86
+from algos import simplicite118 as simp118
+from algos import simplicite154 as simp154
+from algos import simplicite160 as simp160
+from algos import simplicite169 as simp169
+from algos import sobriete102 as sob102
+
+
+
+
+print("|"+eff21.erase("   a     a a  ") + "|")
