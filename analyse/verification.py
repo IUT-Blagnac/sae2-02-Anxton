a22a69016c7d55c23d575f7af9a3eacc14980659
Anxton <anxton4@gmail.com>
2022-06-17 00:56:57 +0200

    ajout test verification python

--- analyse/verification.py
+++ analyse/verification.py
@@ -7,31 +7,32 @@ from algos.simplicite160 import bonmot2 as simp160
 from algos.simplicite169 import erase as simp169
 from algos.sobriete102 import erase as sob102
 
-def sample_tests(erase):
+def test(erase):
     try:
         ok = True
         if erase('') != '': ok = False
         if erase('   06   07 65 19 70 ') != '   06   07651970': ok = False
         if erase(' 666, the number of the beast  ') != '666,thenumberofthebeast  ': ok = False
         if erase('  Cou cou  J M  B') != '  Coucou  JM  B': ok = False
+        if erase(' a a a a ') != 'aaaa': ok = False
         if ok: print('✅') # Fonctionne !
         else: print('❌') # Ne fonctionne pas comme attendu
     except:
         print("💀") # Erreur levée
 
 print("efficacite21:")
-sample_tests(eff21)
+test(eff21)
 print("efficacite46:")
-sample_tests(eff46)
+test(eff46)
 print("simplicite86:")
-sample_tests(simp86)
+test(simp86)
 print("simplicite118:")
-sample_tests(simp118)
+test(simp118)
 print("simplicite154:")
-sample_tests(simp154)
+test(simp154)
 print("simplicite160:")
-sample_tests(simp160)
+test(simp160)
 print("simplicite169:")
-sample_tests(simp169)
+test(simp169)
 print("sobriete102:")
-sample_tests(sob102)
+test(sob102)
