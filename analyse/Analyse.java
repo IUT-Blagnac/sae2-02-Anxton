e3890457b2dca705ce439aaa7931147a95e3f5dc
Anxton <anxton4@gmail.com>
2022-06-17 17:47:48 +0200

    final test efficacité

--- analyse/Analyse.java
+++ analyse/Analyse.java
@@ -14,24 +14,25 @@ public class Analyse {
         StringBuilder sb = new StringBuilder();
         Random rand = new Random();
 
-        int longueur = 200000;
+        int longueur = 500;
         for (int i = 0; i < longueur; i++) {
             sb.append(alphabet.charAt(rand.nextInt(alphabet.length())));
         }
         String chaine = sb.toString();
 
+        Instant inst1, inst2;
 
-        Instant inst1 = Instant.now();
+        inst1 = Instant.now();
         Efficacite29.erase(chaine);
-        Instant inst2 = Instant.now();    
+        inst2 = Instant.now();    
         
-        System.out.println("Efficacite29 : "+ Duration.between(inst1, inst2).toString());
+        System.out.println("Efficacite29 : "+ Duration.between(inst1, inst2).toMillis());
         
         inst1 = Instant.now();
         Efficacite135.erase(chaine);
         inst2 = Instant.now();
 
-        System.out.println("Efficacite135 : "+ Duration.between(inst1, inst2).toString());
+        System.out.println("Efficacite135 : "+ Duration.between(inst1, inst2).toMillis());
         
     }
 }
