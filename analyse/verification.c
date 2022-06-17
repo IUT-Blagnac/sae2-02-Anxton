7f6724c33279aa41d766b22cc90dfb9b43751c97
Anxton <anxton4@gmail.com>
2022-06-17 02:09:36 +0200

    changement test

--- analyse/verification.c
+++ analyse/verification.c
@@ -4,8 +4,9 @@
 
 int main(int argc, char const *argv[])
 {
+    char* string = erase("  b o n j  o u r  ");
     printf("sobriete76:\n");
-    printf("|%s|\n", erase("  b o n j  o u r  "));
-    printf("strcmp: %d", strcmp(erase("  b o n j  o u r  "), "  bonj  our  "));
+    printf("|%s|\n", string);
+    printf("strcmp: %d", strcmp(string, "  bonj  our  "));
     return 0;
 }
