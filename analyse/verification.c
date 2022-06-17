e5584f49f22ae2c3654c4a10f351828102f44f64
Anxton <anxton4@gmail.com>
2022-06-17 02:09:00 +0200

    changement test c

--- analyse/verification.c
+++ analyse/verification.c
@@ -1,9 +1,11 @@
 #include "algos/sobriete76.h"
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
     printf("sobriete76:\n");
-    printf("|%s|", erase("  bo  n jour  "));
+    printf("|%s|\n", erase("  b o n j  o u r  "));
+    printf("strcmp: %d", strcmp(erase("  b o n j  o u r  "), "  bonj  our  "));
     return 0;
 }
